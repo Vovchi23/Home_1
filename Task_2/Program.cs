f9497bdbb3faef17f9c1ce0950e0232f5679a763
Vivchik <vovka.azik@mail.ru>
2022-12-22 19:07:27 +0300

    Task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите ПЕРВОЕ целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+
+Console.Write("Введите ВТОРОЕ целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int number2 = int.Parse(userInput2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} ---> Максимальное!!!");
+}
+else
+
+    Console.WriteLine($"Число {number2} ---> Максимальное!!!");
+
